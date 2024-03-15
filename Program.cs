a212cf0040343666b88af9a10fd142a390161562
Ruslan <Ruslan@WIN-HLVM7BNLTHP>
2024-03-15 23:35:36 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealTimeDeviceSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vision> fdList = new List<Vision>();
+            for (int i=0; i<10; i++)
+            {
+                Random random = new Random();
+                Vision fd = new Vision();
+                fd.FillRobot($"Датчик#{i}", "", "КЗ1", i, random.Next(0, 100));
+                fdList.Add(fd);
+            }
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(2);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                foreach(var fd in fdList)
+                {
+                    fd.ChangeRobotState();
+                    Console.WriteLine($"Состояние датчика {fd.Name} - Статус: {fd.Status}, Кол-во Объектов - {fd.ObjectQuantity}");
+                }
+            }, null, startTimeSpan, periodTimeSpan);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vision.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace RealTimeDeviceSystem
+{
+    public class Vision
+    {
+        public static string[] StatusList = { "Работает", "Выключен", "Объект_найден" };
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Model { get; set; }
+        public Location Location { get; set; }
+
+        public int ObjectQuantity{ get; set; }
+        public string Status { get; set; }
+
+        public void FillRobot(string name, string description, string model, int robotNumber, int coord)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Model = model;
+            Location loc = new Location();
+            loc.FillLocation(robotNumber, coord);
+            this.Location = loc;
+        }
+
+        public void ChangeRobotState()
+        {
+            Random random = new Random();
+            this.ObjectQuantity = random.Next(0, 11);
+            this.Status = StatusList[random.Next(0, 3)];
+        }
+    }
+
+    public class Location
+    {
+        public int RobotNumber { get; set; }
+        public int Coord { get; set; }
+        public int ObjectType { get; set; }
+
+        public void FillLocation(int robotNumber, int coord)
+        {
+            this.RobotNumber = robotNumber;
+            this.Coord = coord;
+        }
+    }
+}
